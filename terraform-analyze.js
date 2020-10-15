9e5ffde2de3dc220e314f8f7b7f87b9062ffc918
Elay Gliksberg <elaygl@gmail.com>
2020-10-15 14:46:41 +0300

    added colors to table

--- terraform-analyze.js
+++ terraform-analyze.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { table } = require('table')
+const chalk = require('chalk');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
@@ -15,6 +16,12 @@ const parseTerraformTime = (tfTime) => {
         else if (unit === 's') timeValue += parseInt(timePeace);
     })
     return timeValue;
+}
+
+const getTimeColor = (parsedTime) => {
+    if (parsedTime > 10 * 60) return chalk.red;
+    if (parsedTime > 5 * 60) return chalk.yellow;
+    return chalk.green;
 }
 
 const analyzeTerraformOutput = (output) => {
@@ -34,11 +41,13 @@ const analyzeTerraformOutput = (output) => {
     const entries = Object.entries(moduleToTimeMap);
     if (!entries.length) return;
 
-    const parsedEntries = entries.map(entry => [entry[0], { parsed: parseTerraformTime(entry[1]), elapsed: entry[1] }]);
+    const parsedEntries = entries
+        .map(entry => [entry[0], { parsed: parseTerraformTime(entry[1]), elapsed: entry[1] }])
+        .filter(entry => entry[1].parsed > 10);
     parsedEntries.sort((a, b) => b[1].parsed - a[1].parsed)
     const tableData = [
         ['Module Name', 'Time'],
-        ...parsedEntries.map(entry => [entry[0], entry[1].elapsed])
+        ...parsedEntries.map(entry => [entry[0], getTimeColor(entry[1].parsed)(entry[1].elapsed)])
     ];
     console.log('Top long resource creation:')
     console.log(table(tableData));
